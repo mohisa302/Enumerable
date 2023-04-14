bafbcc58928fe7f7c41917e2eb8aaefe8a84a65a
mohi <mohisadeghi302@gmail.com>
2023-04-14 18:18:13 +0430

    Introduce bug

--- class.rb
+++ class.rb
@@ -4,10 +4,10 @@ class MyList
   include MyEnumerable
 
   def initialize(*list)
-    @list = list
+    list = list
   end
 
   def each(&block)
-    @list.each(&block)
+    @list.each(block)
   end
 end
